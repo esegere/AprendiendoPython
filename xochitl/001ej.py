121d5a024c547f1c98640f151feeab55b3d3face
XOCHITL <xoch.miranda.05@gmail.com>
2021-01-07 21:41:06 -0600

    primer ejercicio

--- xochitl/001ej.py
+++ xochitl/001ej.py
@@ -1,6 +1,19 @@
 def main():
 # crear una variable de cada tipo e imprimir su valor
-    pass
+
+    xulo x_a= True
+    yanke y_w = False
+
+    bolean bo= 21
+    float flo= 5.0
+
+
+
+    string mu= "hola mundo "
+    print (mu)
+
+
+
 
 
 
