1827966850cae34f4af8d4deaca5d6a8d72baeed
Jessica <jessy.r.m.viaje@outlook.es>
2021-01-07 21:45:50 -0600

    hice una barbaridad de ejercicio

--- Jessica/ejercicio1.py
+++ Jessica/ejercicio1.py
@@ -1,7 +1,30 @@
 def main():
 # crear una variable de cada tipo e imprimir su valor
-    pass
+    #booleanos
+    yo_te_amo = True
+    yo_no_te_amo = False
 
+    #Entero positivo y negativo
+    inicio_amor = 100
+    limite_de_amor = -100
+    cuanto_te_amo = 1_000_000_000_000_000_000
+
+    #cosas raras que ya se me olvidaron
+    bin = 0b11
+    octal = 0o11
+    hexad = 0x11
+
+    #flotantes
+    flotante = 54.5
+    notacion_cientifica = 54e-20
+
+    #string
+    comilla_simple = 'Te amo Sergio'
+    comilla_doble = "Te amo x3"
+    comilla_simple_doble = 'Te amo bebe "Sergio"'
+    comilla_doble_simple = "Eres el mejor 'mi vida'"
+
+    print(yo_te_amo)
 
 
 if __name__ == "__main__":
