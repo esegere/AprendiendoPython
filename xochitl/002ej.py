29daa767d35018c19e7430ca0a856a9b16e0c57a
XOCHITL <xoch.miranda.05@gmail.com>
2021-01-10 20:09:51 -0600

    segundo ejercicio

--- xochitl/002ej.py
+++ xochitl/002ej.py
@@ -1,8 +1,8 @@
 # agregar en una lista los numeros que introdzca el usuario
 # y corregir los errores que se presenten
 if __name__ == '__main__':
-    lista_numeros =  # declarar una lista vacia
+    lista_numeros = [] # declarar una lista vacia
     while true:
         nuevo_dato = input("ingesa un numero: ")
-        lista_numeros.  # usar el metodo adecuado para ingresar los numeros
+        lista_numeros []=[1,2,3,4,5,6,7,8,9] # usar el metodo adecuado para ingresar los numeros
         print(lista_numeros)
