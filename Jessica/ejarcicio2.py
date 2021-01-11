26d7e207e7055754081f529cf1126ada317ef75b
Jessica <jessy.r.m.viaje@outlook.es>
2021-01-10 20:11:49 -0600

    este ejercicio tiene otro mensajito, despues del 19, los numeros son letras del telefono

--- Jessica/ejarcicio2.py
+++ Jessica/ejarcicio2.py
@@ -1,8 +1,8 @@
 # agregar en una lista los numeros que introdzca el usuario
 # y corregir los errores que se presenten
 if __name__ == '__main__':
-    lista_numeros =  # declarar una lista vacia
-    while true:
+    lista_numeros = [] # declarar una lista vacia
+    while True:
         nuevo_dato = input("ingesa un numero: ")
-        lista_numeros.  # usar el metodo adecuado para ingresar los numeros
+        lista_numeros.append(nuevo_dato) # usar el metodo adecuado para ingresar los numeros
         print(lista_numeros)
