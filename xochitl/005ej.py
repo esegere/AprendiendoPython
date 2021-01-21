a0844fbfff7c88b2ac211400516cbaa110e78b76
XOCHITL <xoch.miranda.05@gmail.com>
2021-01-20 22:43:48 -0600

    ejercico cinco

--- xochitl/005ej.py
+++ xochitl/005ej.py
@@ -35,3 +35,78 @@
 #   PISTA: como es costumbre mia, la colección predilecta para este ejercicio es el diccionario
 #   NOTA: para que las funciones puedan usar el diccionario de la agenda, pasarlo por parámetro
 #   PETICIÓN: para facilitar la estructura del código crear funciones, no hay limite, el criterio es suyo
+
+AGENDA="""
+   1.-Consultar numero de contacto
+   2.-Agregar contacto
+   3.-Eliminar Contactos 
+   4.-Modificar numero de contacto
+   5.- Salir 
+   
+"""
+def buscar_nombres(dic_agenda):
+    nombre=input("Ingrese un nombre:")
+
+    if nombre in dic_agenda.keys():
+        numero_contacto=dic_agenda[nombre]
+
+        print(f"el numero de la {nombre} es {numero_contacto}")
+    else:
+        print(f"{nombre} no registrada en tu agenda:")
+
+def agregar_contactos(dic_agenda):
+     nombre= input("Ingrese el nombre del usuario de su contacto nuevo:")
+     numero=input("Ingrese el numero con 10 digitos:")
+     dic_agenda[nombre]=numero
+
+def eliminar_contacto(dic_agenda):
+    nombre=input("Nombre:")
+
+    if nombre in dic_agenda.keys():
+        del dic_agenda[nombre]
+        print(f"{nombre} fue eliminado:")
+    else:
+        print(f"{nombre} no esta registrada en tu agenda:")
+
+
+def modificar_contactos(dic_agenda):
+    nombre=input("Buscar el nombre:")
+
+    if nombre in dic_agenda.keys():
+        numero=input("Ingrese el nuevo numero:")
+        numero_anterior=dic_agenda[nombre]
+
+        dic_agenda[nombre]=numero
+
+        print(f"Numero de {nombre} ah sido cambiado de {numero_anterior} a {numero} ")
+    else:
+        print(f"{nombre} no registrada en tu agenda:")
+
+
+
+if __name__=='__main__':
+
+    dic_agenda = {}
+
+
+
+    while True:
+        print(AGENDA)
+        option = input("ingrese una opción")
+        if option == "1":
+            buscar_nombres(dic_agenda)
+        elif option == "2":
+            agregar_contactos(dic_agenda)
+        elif option == "3":
+            eliminar_contacto(dic_agenda)
+        elif option == "4":
+            buscar_nombres(dic_agenda)
+        elif option == "5":
+            print("Salir")
+
+            break
+        else:
+            print("Opcion no valida")
+
+
+
