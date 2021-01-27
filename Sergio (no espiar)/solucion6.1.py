6c7160ff59652a09e8a3017e99bd255b7a174462
Sergio <garciasergiomar@gmail.com>
2021-01-27 17:20:09 -0600

    agregado ejercicio para mi bebe

---
+++ Sergio (no espiar)/solucion6.1.py
@@ -0,0 +1,8 @@
+# escriba un programa que pida al usuario una lista de números enteros separados por coma
+#               similar al ejercicio 4
+# después ofrezca las siguientes opciones
+#
+# 1) imprimir números pares
+# 2) imprimir números impares
+# 3) imprimir números primos
+# 4) salir
