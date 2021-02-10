88f5cb9eb38593e7617f27411f495e2f3dd5fc01
Sergio <garciasergiomar@gmail.com>
2021-02-09 19:03:00 -0600

    agregado repaso y ejercicio conjunto

---
+++ Sergio (no espiar)/repaso.py
@@ -0,0 +1,30 @@
+# 4 tipos de datos
+_string = "string"
+_int = 5
+_float = 5.8
+_bool = True or False
+
+# 3 colecciones principales
+
+diccionario = {}
+lista = []
+tupla = ()
+
+
+# declarar funciones con parámetros y valores de retorno
+
+def funcion(parametro):
+    return parametro * 2
+
+
+# declarar funciones sin argumentos
+
+def funcion_s():
+    print("print")
+
+
+# declarar una clase con su constructor
+
+class Clase:
+    def __init__(self):
+        self.atributo = 5
