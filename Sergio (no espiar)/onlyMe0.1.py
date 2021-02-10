d69040173261a39dfbce22c6667feb242cc79934
Sergio <garciasergiomar@gmail.com>
2021-02-09 19:03:00 -0600

    agregado repaso y ejercicio conjunto

---
+++ Sergio (no espiar)/onlyMe0.1.py
@@ -0,0 +1,8 @@
+# realizar un programa que ueda calcular:
+#   - promedio
+#   - moda
+#   - valor maximo
+#   - valor minimo
+#
+# el programa debe pedir una lista de números desde la linea de comandos
+# y regresar los resultados de todas las operaciones
