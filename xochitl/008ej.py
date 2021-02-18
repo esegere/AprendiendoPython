146bd7486590d979af8baa49adcfb358a0a41065
Sergio <garciasergiomar@gmail.com>
2021-02-17 19:33:48 -0600

    agregado ejercicio 8

---
+++ xochitl/008ej.py
@@ -0,0 +1,6 @@
+# escriba un programa qué acepte como argumento una cadena de texto
+# y transforme la cadena de acuerdo a las siguientes banderas
+#
+# -r        invierta la cadena de texto
+# -fN       imprima solo las primeras N letras de la cadena
+# -lN       imprimir solo las ultimas N letras de la cadena
