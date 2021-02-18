9685b12cbc621156e7052ca66ab7afeee16cfbdc
XOCHITL <xoch.miranda.05@gmail.com>
2021-02-17 21:50:43 -0600

    septimo ejercico

--- xochitl/007ej.py
+++ xochitl/007ej.py
@@ -5,7 +5,24 @@
 # para hacer el conteo, use la clase Counter del modulo collections
 #
 
-from sys import argv
+from sys import argv as parametro
+from collections import Counter
+
+def validar_parametro(parametro):
+    if len(parametro)>=2:
+        return True
+    else:
+        print("el parametro no es correcto ya que solo se pueden validar las funciones de acuerdo a mayor que 2 valores :(")
+        return False
+
+def contar_palabras(parametro):
+      contador=Counter(parametro.replace(",","").split(" "))
+      for palabra, veces in contador.items():
+       print(f"{palabra}:{veces}")
 
 if __name__ == '__main__':
-    pass
+ if   validar_parametro(parametro):
+      contar_palabras(parametro[1])
+
+
+
