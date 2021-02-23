3d295b110174fa3cafc36c4edf49273919094e19
Sergio <garciasergiomar@gmail.com>
2021-02-23 17:47:34 -0600

    ejercicios agregados

---
+++ xochitl/009ej.py
@@ -0,0 +1,6 @@
+# En el siguiente ejercicio replicaremos el comportamiento del programa del ejercicio4
+#
+# Pero esta vez usaremos la funcion reduce del modulo functools
+# para poder hacer las operaciones
+#
+# De la misma forma, usaremos decoradores para saber en que momento llamar las funciones
