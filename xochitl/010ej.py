d50fb0f1029bcca9cb96f447ea0099a8a6c9fd0b
Sergio <garciasergiomar@gmail.com>
2021-03-01 18:19:00 -0600

    ejercicio 10 agregado

---
+++ xochitl/010ej.py
@@ -0,0 +1,29 @@
+# cree un programa que funcione desde linea de commandos
+#       mediante argv
+# al programa se le debe dar com parámetro:
+#           una frase
+#           un numero
+#
+# en ese orden
+#
+# el programa debera usar las letras de la frase para construir un cuadrado N de lado
+# N siendo el numero pasado por parámetro
+#
+# si la frase no alcanza, concatenar la frase una vez mas, si excede recortarla
+
+# ejemplo:
+#
+# python programa "frase de prueba" 8
+
+# resultado:
+#
+#   frase de
+#   pruebafr
+#   ase de p
+#   ruebafra
+#   se de pr
+#   uebafras
+#   e de pru
+#   ebafrase
+
+# escribir un generador que proporcione las letras de la frase indefinidamente y la repita si es necesario
